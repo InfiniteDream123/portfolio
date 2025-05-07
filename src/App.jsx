38096280ef7b040eda3eb71dd5def82e403d472b
InfiniteDream123 <infinite.dev.999@hotmail.com>
2025-05-07 06:30:32 -0500

    fex: remove cotact info

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
